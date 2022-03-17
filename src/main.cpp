71275b9c143c5817ccc7e0d1b05a9c8ed8dc877d
Lewis Hamilton <vk2gzz@ieee.org>
2022-03-17 15:30:19 +1100

    Added calibration data from eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,15 @@
 
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "Adafruit_SSD1306.h"
 
 #define TONE_OUT 9
 #define POT_IN A0
 
 #define PWM_PRESCALER 8
-#define CPU_FREQUENCY 16000000
+// CPU Frequency will be callibrated and stored in EEPROM.
+//#define CPU_FREQUENCY 16000000
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
@@ -26,17 +28,31 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+unsigned long int CPU_FREQUENCY = 0;
 
 
 void OutputTone(float frequency);
 void UpdateLCD(float frequency);
 int ReadPot();
 
-float const lookupArray[] = {67,69.3,71.9,74.4,77,79.7,82.5,85.4,88.5,91.5,94.8,97.4,100,103.5,107.2,110.9,114.8,118.8,123,127.3,131.8,136.5,141.3,146.2,151.4,156.7,162.2,167.9,173.8,179.9,186.2,192.8,203.5,210.7,218.1,225.7,233.6,241.8,250.3};
+
+unsigned long EepromReadLong(int address);
+void EepromWriteLong(int address, unsigned long value);
+void EepromWriteInt(int address, int value);
+unsigned int EepromReadInt(int address);
+
+float const lookupArray[] = {67,69.3,71.9,74.4,77,79.7,82.5,85.4,88.5,91.5,
+                             94.8,97.4,100,103.5,107.2,110.9,114.8,118.8,
+                             123,127.3,131.8,136.5,141.3,146.2,151.4,156.7,
+                             162.2,167.9,173.8,179.9,186.2,192.8,203.5,210.7,
+                             218.1,225.7,233.6,241.8,250.3};
 
 
 void setup()
 {
+    // Load in calibrated cpu frequency first.
+    CPU_FREQUENCY = EepromReadLong(0x10);
+
     pinMode(TONE_OUT, OUTPUT);
     pinMode(POT_IN, INPUT);
     pinMode(13, OUTPUT);
@@ -57,7 +73,18 @@ void setup()
     display.cp437(true);
     display.print(F("TU8K"));
     display.display();
-    delay(1000);
+    delay(500);
+    if(!CPU_FREQUENCY)
+    {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0,0);
+        display.print(F("FREQ NOT CALIBRATED!\nSETTING TO 16MHz"));
+        display.display();
+        CPU_FREQUENCY = 16000000;
+        display.setTextSize(3);
+        delay(1000);
+    }
 
 }
 
@@ -92,4 +119,32 @@ void UpdateLCD(float frequency)
     display.setCursor(0,0);
     display.print(frequency, 1);
     display.display();
+}
+
+
+unsigned long EepromReadLong(int address)
+{
+    unsigned long word = EepromReadInt(address);
+    word = word << 16;
+    word = word | EepromReadInt(address+2);
+    return word;
+}
+
+void EepromWriteLong(int address, unsigned long value) 
+{
+    EepromWriteInt(address+2, word(value));
+    value = value >> 16;
+    EepromWriteInt(address, word(value));
+}
+
+void EepromWriteInt(int address, int value) 
+{
+    EEPROM.write(address,highByte(value));
+    EEPROM.write(address+1 ,lowByte(value));
+}
+
+unsigned int EepromReadInt(int address) 
+{
+    unsigned int word = word(EEPROM.read(address), EEPROM.read(address+1));
+    return word;
 }
